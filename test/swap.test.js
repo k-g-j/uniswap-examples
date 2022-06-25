b7d4da66a5286d2a0ebb37de5153448ffa973818
Kate Johnson <johnson.kat.g@gmail.com>
2022-06-25 18:39:47 -0400

    swapping examples

---
+++ test/swap.test.js
@@ -0,0 +1,70 @@
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
+
+const DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+const WETH9 = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+const USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+
+describe('SwapExamples', function () {
+  let swapExamples, accounts, weth, dai
+  before(async () => {
+    accounts = await ethers.getSigners()
+
+    weth = await ethers.getContractAt('IWETH', WETH9)
+    dai = await ethers.getContractAt('IERC20', DAI)
+
+    const SwapExamples = await ethers.getContractFactory('SwapExamples')
+    swapExamples = await SwapExamples.deploy()
+    await swapExamples.deployed()
+  })
+
+  it('swapExactInputSingle', async function () {
+    const amountIn = 10n ** 18n
+
+    await weth.connect(accounts[0]).deposit({ value: amountIn })
+    await weth.connect(accounts[0]).approve(swapExamples.address, amountIn)
+
+    await swapExamples.swapExactInputSingle(amountIn)
+
+    console.log('DAI balance', await dai.balanceOf(accounts[0].address))
+  })
+
+  it('swapExactOutputSingle', async function () {
+    const wethAmountInMax = 10n ** 18n
+    const daiAmountOut = 100n * 10n ** 18n
+
+    await weth.connect(accounts[0]).deposit({ value: wethAmountInMax })
+    await weth
+      .connect(accounts[0])
+      .approve(swapExamples.address, wethAmountInMax)
+
+    await swapExamples.swapExactOutputSingle(daiAmountOut, wethAmountInMax)
+
+    console.log('DAI balance', await dai.balanceOf(accounts[0].address))
+  })
+
+  it('swapExactInputMultihop', async function () {
+    const amountIn = 10n ** 18n
+
+    await weth.connect(accounts[0]).deposit({ value: amountIn })
+    await weth.connect(accounts[0]).approve(swapExamples.address, amountIn)
+
+    await swapExamples.swapExactInputMultihop(amountIn)
+
+    console.log('DAI balance', await dai.balanceOf(accounts[0].address))
+  })
+
+  it('swapExactOutputMultihop', async function () {
+    const wethAmountInMax = 10n ** 18n
+    const daiAmountOut = 100n * 10n ** 18n
+
+    await weth.connect(accounts[0]).deposit({ value: wethAmountInMax })
+    await weth
+      .connect(accounts[0])
+      .approve(swapExamples.address, wethAmountInMax)
+
+    await swapExamples.swapExactOutputMultihop(daiAmountOut, wethAmountInMax)
+
+    console.log('DAI balance', await dai.balanceOf(accounts[0].address))
+  })
+})
